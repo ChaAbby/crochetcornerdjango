0731a42bc6c7d59cfd3dc832152f81d54ad51680
ChaAbby <abigail.charime@gmail.com>
2024-07-19 17:49:12 -0700

    load env"

--- crochetcornerdjango/settings.py
+++ crochetcornerdjango/settings.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 """
 Django settings for crochetcornerdjango project.
 
@@ -12,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
